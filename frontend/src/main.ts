97a5d8389c1a0fd1ac89e3934b856e90b4cff068
lisamaliqi <lisamaliqi@hotmail.com>
2025-02-26 14:41:27 +0100

    adding grid system and displaying virus in a random position in the grid system

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -11,9 +11,59 @@ console.log("🙇 Connecting to Socket.IO Server at:", SOCKET_HOST);
 // Connect to Socket.IO Server
 const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(SOCKET_HOST);
 
+
+/**
+ * ELEMENTS
+ */
+const gridContainerEl = document.querySelector('#grid-container') as HTMLDivElement;
+
+
+/**
+ * DISPLAY VIRUS IN RANDOM POSITION VARIABLE
+*/
+
+const gridSize = 10; // 10x10 grid
+const totalCells = gridSize * gridSize;
+
+
+/**
+ * CREATE GRID
+*/
+for (let i = 1; i <= totalCells; i++) {
+	gridContainerEl.innerHTML += `<div class="cells">${i}</div>`;
+};
+
+
+/**
+ * FUNCTIONS
+ */
+
+function placeObject() {
+	const cellsEl = document.querySelectorAll(".cells");
+	
+	// Remove any other object (number and virus) from the grid
+	cellsEl.forEach(cell => cell.innerHTML = "");	
+	
+	// Calculate the random position of the virus
+    const randomIndex = Math.floor(Math.random() * totalCells);
+
+	// Place the virus at the random index
+	cellsEl[randomIndex].innerHTML = "<span class='object'>🦠</span>";
+	
+	// When user clicks on the object, restart the function and place the object at a new random position
+	const objectEl = document.querySelector('.object') as HTMLDivElement;
+	objectEl.addEventListener('click', placeObject);
+};
+
+
+
+
+
+
+
 /**
  * Socket Event Listeners
- */
+*/
 
 // Listen for when connection is established
 socket.on("connect", () => {
@@ -30,3 +80,11 @@ socket.on("disconnect", () => {
 socket.io.on("reconnect", () => {
 	console.log("😊 Reconnected to server:", socket.io.opts.hostname + ":" + socket.io.opts.port);
 });
+
+
+
+
+
+
+// Initialize the game with placing a virus at a random position
+placeObject();
