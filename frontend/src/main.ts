ca417e97a9b2261871f3dbded77c323ce8359154
lisamaliqi <lisamaliqi@hotmail.com>
2025-03-14 09:18:25 +0100

    putting virus as the object, again

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -93,7 +93,7 @@ const placeObject = (position: number) => { //Place virus on grid
 	cellsEl.forEach(cell => cell.innerHTML = "");
 
 	//Place the virus at the random index, index is taken from "virusPosition" socket event
-	cellsEl[position].innerHTML = "<span class='object'><img src='/public/assets/img/ledsen-mans2.0.png' width='100px' alt='sad måns'></span>";
+	cellsEl[position].innerHTML = "<span class='object'>🦠</span>";
 
 	//start timer
 	timerStart = Date.now();
